c8c1d6c0836f0556b930e5a4aa5a180e92a2f4e0
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-06-26 20:21:38 +0300

    changed type handling

--- main.cpp
+++ main.cpp
@@ -1,162 +1,73 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <cstdlib>
+#include <sstream>
+#include <fstream>
 
 class TextContainer {
 public:
-    TextContainer();
-    ~TextContainer();
-    char* buffer;
-    int currentSize;
-    int capacity;
-    void reallocate();
-    void checkCapacity(int additionalSize);
-    void clearBuffer();
+    TextContainer(const char* initialData = "") {
+        length = std::strlen(initialData);
+        data = new char[length + 1];
+        std::strcpy(data, initialData);
+    }
+
+    ~TextContainer() {
+        delete[] data;
+    }
+
+    size_t size() const {
+        return length;
+    }
+
+    const char* c_str() const {
+        return data;
+    }
+
+    void setData(const char* newData) {
+        delete[] data;
+        length = std::strlen(newData);
+        data = new char[length + 1];
+        std::strcpy(data, newData);
+    }
+
+    void print() const {
+        std::cout << data << std::endl;
+    }
+private:
+    char* data;
+    size_t length;
 };
 
 class TextEditor {
 public:
-    TextEditor(TextContainer& text);
-    ~TextEditor();
-    void saveToFile(TextContainer& container);
-    TextContainer loadFromFile();
-private:
-    TextContainer textBuffer;
-};
+    void loadFromFile(std::string filename, TextContainer& textContainer) {
+        std::ifstream file(filename);
+        if (file) {
+            file.seekg(0, std::ios::end);
+            size_t length = file.tellg();
+            file.seekg(0, std::ios::beg);
 
-TextContainer::TextContainer() : currentSize(0), capacity(100) {
-    buffer = new char[capacity];
-    if (!buffer) {
-        std::cerr << "Memory allocation failed" << std::endl;
-        std::exit(1);
-    }
-    buffer[0] = '\0';
-}
+            char* buffer = new char[length + 1];
+            file.read(buffer, length);
+            buffer[length] = '\0';
 
-TextContainer::~TextContainer() {
-    delete[] buffer;
-}
+            textContainer.setData(buffer);
 
-void TextContainer::reallocate() {
-    capacity += 100;
-    char* newBuffer = new char[capacity];
-    if (!newBuffer) {
-        std::cerr << "Memory allocation failed" << std::endl;
-        std::exit(1);
-    }
-    std::memcpy(newBuffer, buffer, currentSize);
-    delete[] buffer;
-    buffer = newBuffer;
-}
-
-void TextContainer::checkCapacity(int additionalSize) {
-    while (currentSize + additionalSize >= capacity) {
-        reallocate();
-    }
-}
-
-void TextContainer::clearBuffer() {
-    delete[] buffer;
-    buffer = new char[capacity];
-    if (!buffer) {
-        std::cerr << "Memory allocation failed" << std::endl;
-        std::exit(1);
-    }
-    currentSize = 0;
-    buffer[0] = '\0';
-}
-
-TextEditor::TextEditor(TextContainer& text) {
-    textBuffer.buffer = new char[text.currentSize + 1];
-    std::memcpy(textBuffer.buffer, text.buffer, text.currentSize);
-    textBuffer.currentSize = text.currentSize;
-    textBuffer.capacity = text.currentSize;
-}
-
-TextEditor::~TextEditor() {
-    textBuffer.clearBuffer();
-}
-
-void TextEditor::saveToFile(TextContainer& container) {
-    char fileName[30];
-    std::cout << "Enter the name of the file you want to save to. Note that if there is something in this file, your text would be append to the end" << std::endl;
-    std::cin >> fileName;
-    FILE* file = fopen(fileName, "a");
-    if (!file) {
-        std::cerr << "Failed to open file" << std::endl;
-        return;
-    }
-    fwrite(container.buffer, sizeof(char), container.currentSize, file);
-    fwrite("\n", sizeof(char), 1, file);
-    fclose(file);
-    std::cout << "Text saved to file" << std::endl;
-}
-
-TextContainer TextEditor::loadFromFile() {
-    TextContainer container;
-
-    char fileName[30];
-    std::cout << "Enter the name of the file you want to load from" << std::endl;
-    std::cin >> fileName;
-    FILE* file = fopen(fileName, "r");
-    if (!file) {
-        std::cerr << "Failed to open file" << std::endl;
-        return container;
-    }
-
-    container.clearBuffer();
-
-    char c;
-    while ((c = getc(file)) != EOF) {
-        container.checkCapacity(1);
-        container.buffer[container.currentSize++] = c;
-    }
-    container.buffer[container.currentSize] = '\0';
-
-    fclose(file);
-    std::cout << "Text loaded from file" << std::endl;
-
-    return container;
-}
-
-int getCommand() {
-    int command;
-    std::cout << "Enter 1 to encrypt" << std::endl;
-    std::cout << "Enter 2 to decrypt" << std::endl;
-    std::cout << ">>>";
-    std::cin >> command;
-    return command;
-}
-
-int main() {
-    int command;
-    TextContainer textStorage;
-    TextEditor textedit(textStorage);
-    do {
-        textStorage = textedit.loadFromFile();
-        command = getCommand();
-        switch (command) {
-            case 1: {
-                //encrypt
-                std::cout << "e"<<std::endl;
-                break;
-            }
-            case 2:{
-                //decrypt
-                std::cout<< "d"<<std::endl;
-            }
-            case 0: {
-                std::cout << "Exit! Have a good day!" << std::endl;
-                break;
-            }
-            default: {
-                std::cout << "There is no such a command, sorry :(" << std::endl;
-                break;
-            }
+            delete[] buffer;
+            file.close();
+        } else {
+            std::cerr << "Could not open file: " << filename << std::endl;
         }
-        textedit.saveToFile(textStorage);
-        std::cout << "Saved!" << std::endl;
-    } while (command != 0);
-    return 0;
-}
+    }
+
+    void saveToFile(std::string filename, const TextContainer& textContainer) {
+        std::ofstream file(filename);
+        if (file) {
+            file.write(textContainer.c_str(), textContainer.size());
+            file.close();
+        } else {
+            std::cerr << "Could not open file: " << filename << std::endl;
+        }
+    }
+};
